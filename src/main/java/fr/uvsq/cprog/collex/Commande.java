ca54801f11cd9e3ff95e41da9b0232bb2a9da01f
Mouhcene Narimane <mouhcene.narimane@gmail.com>
2025-10-19 20:58:05 +0200

    Ajout de l'interface Commande

---
+++ src/main/java/fr/uvsq/cprog/collex/Commande.java
@@ -0,0 +1,9 @@
+package fr.uvsq.cprog.collex;
+
+/**
+ * Interface pour toutes les commandes DNS.
+ * Chaque commande doit implémenter la méthode execute().
+ */
+public interface Commande {
+    void execute();
+}
